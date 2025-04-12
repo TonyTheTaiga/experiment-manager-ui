63afd82bd2a60d78fb578081e53fb20dbe5ace25
Taiga Ishida <tonythetaiga@gmail.com>
2025-04-12 02:47:39 -0400

    add tailwind config file with colors used throughout the site defined

---
+++ web/tailwind.config.js
@@ -0,0 +1,37 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  theme: {
+    extend: {
+      colors: {
+        ctp: {
+          base: 'var(--color-ctp-base)',
+          mantle: 'var(--color-ctp-mantle)',
+          crust: 'var(--color-ctp-crust)',
+          surface0: 'var(--color-ctp-surface0)',
+          surface1: 'var(--color-ctp-surface1)',
+          surface2: 'var(--color-ctp-surface2)',
+          overlay0: 'var(--color-ctp-overlay0)',
+          overlay1: 'var(--color-ctp-overlay1)',
+          overlay2: 'var(--color-ctp-overlay2)',
+          text: 'var(--color-ctp-text)',
+          subtext0: 'var(--color-ctp-subtext0)',
+          subtext1: 'var(--color-ctp-subtext1)',
+          lavender: 'var(--color-ctp-lavender)',
+          blue: 'var(--color-ctp-blue)',
+          sapphire: 'var(--color-ctp-sapphire)',
+          sky: 'var(--color-ctp-sky)',
+          teal: 'var(--color-ctp-teal)',
+          green: 'var(--color-ctp-green)',
+          yellow: 'var(--color-ctp-yellow)',
+          peach: 'var(--color-ctp-peach)',
+          maroon: 'var(--color-ctp-maroon)',
+          red: 'var(--color-ctp-red)',
+          mauve: 'var(--color-ctp-mauve)',
+          pink: 'var(--color-ctp-pink)',
+          flamingo: 'var(--color-ctp-flamingo)',
+          rosewater: 'var(--color-ctp-rosewater)'
+        }
+      }
+    }
+  }
+};
