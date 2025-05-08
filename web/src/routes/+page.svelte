eb026a7ea8a5c9c9284c4f4e872beb9ee2d8e2c0
Taiga Ishida <tonythetaiga@gmail.com>
2025-05-08 01:30:45 -0400

    update nav for mobile

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -24,16 +24,18 @@
     <nav
       class="px-6 py-4 flex flex-row justify-between bg-ctp-mantle border-b border-ctp-surface0"
     >
-      <div class="w-42 text-ctp-mauve fill-current">
+      <div class="w-32 lg:w-42 text-ctp-mauve fill-current">
         <Logo />
       </div>
       <button
         onclick={() => {
           isOpen = true;
         }}
-        class="inline-flex items-center justify-center gap-2 px-4 py-2 rounded-md bg-ctp-mauve text-ctp-crust hover:bg-ctp-lavender transition-colors font-medium"
+        class="inline-flex items-center justify-center sm:gap-2 sm:px-4 sm:py-2 w-10 h-10 sm:w-auto sm:h-auto rounded-full sm:rounded-md bg-ctp-mauve text-ctp-crust hover:bg-ctp-lavender transition-colors font-medium"
+        aria-label="New Experiment"
       >
-        <Plus size={16} /> New Experiment</button
+        <Plus size={16} />
+        <span class="hidden sm:inline">New Experiment</span></button
       >
     </nav>
   </header>
